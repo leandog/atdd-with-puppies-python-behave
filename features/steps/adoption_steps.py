aeec280feef184223666de8dbc07c4ec948f49f0
Julias Shaw <julias.shaw@gmail.com>
2025-05-01 13:04:21 -0700

    Fixed false positive in 'I see 'text' step'

--- features/steps/adoption_steps.py
+++ features/steps/adoption_steps.py
@@ -52,6 +52,6 @@ def step_impl(context):
             row['expected_value']
         )
 
-@step('I see "Thank you for adopting a puppy!"')
-def step_impl(context):
-    expect (context.page.locator(f"//p[contains(text(), 'Thank you for adopting a puppy!')]"))
+@step('I see "{expected_text}"')
+def step_impl(context, expected_text):
+    expect(context.page.get_by_text(expected_text)).to_be_visible()
